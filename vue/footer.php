67e9cc923765e4a38ce6fb90b93cf1860a9c0854
jfliaud <jeffliaud@MacBook-Pro-de-Jean-Fernand.local>
2017-02-07 20:30:00 +0100

    modification footer

--- vue/footer.php
+++ vue/footer.php
@@ -2,6 +2,5 @@
     <div class="col-2 col-offset-5 col-m-2 col-m-offset-5 col-l-2 col-l-offset-5" style="text-align: center; margin-top:50px; margin-bottom: 50px; position:relative; cursor: pointer;">
 
         <img class="col-img rotat" src="image/fcircle.png" class="col-img">
-        <img class="col-img rotat" src="image/fcircle.png" class="col-img">
     </div>
 </div>
