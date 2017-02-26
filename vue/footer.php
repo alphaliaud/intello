d385d81369a00b6ccce77bbe41f8494f37d86f3e
Liaud jean-louis <jlliaud@mesoigner.fr>
2017-02-26 21:49:38 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit f695955ae1faea9a6a42f1ea789de2a220c092c2, reversing
    changes made to 3cb134ee87d7e0b692bdcc412640d4365674173e.

--- vue/footer.php
+++ vue/footer.php
@@ -1,11 +1,5 @@
 <div class="footerh">
-<<<<<<< HEAD
     <div class="col-xs-4 col-xs-offset-4" style="text-align: center">
         <img class="col-img rotat" width="200px" height="200px" src="image/fcircle.png" class="col-img">
-=======
-    <div class="col-2 col-offset-5 col-m-2 col-m-offset-5 col-l-2 col-l-offset-5" style="text-align: center; margin-top:50px; margin-bottom: 50px; position:relative; cursor: pointer;">
-
-        <img class="col-img rotat" src="image/fcircle.png" class="col-img">
->>>>>>> origin/master
     </div>
 </div>
